f2ff060678c28b8f83178fb79219d03ff87de805
ankittejyadav <ankittejyadav@gmail.com>
2023-07-23 19:29:49 -0400

    CORS error with fetching data

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import api from './api/axiosConfig';
-import { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [movies, setMovies] = useState();
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch {
+      console.log();
+    }
+  };
 
-  const [movies,setMovies]=useState;
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
